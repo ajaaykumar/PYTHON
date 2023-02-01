4cce95a99406c77bfaae33ac9925ac0cb5c4123b
ajaaykumar <ajaybhangay@gmail.com>
2023-02-01 23:03:19 +0530

    Added some random question

---
+++ Python-Coding-Challenges/Some_Random_Challenges.py
@@ -0,0 +1,14 @@
+# Write a program to find the first non-repeated character in a string
+# Write a program to check if two strings are anagrams of each other
+# Write a program to find the longest substring without repeating characters
+# Write a program to find the longest common prefix among a set of strings
+# Write a program to remove a specific character from a string
+# Write a program to implement the Rabin-Karp algorithm for string matching
+# Write a program to convert a string to its equivalent integer
+# Write a program to replace a specific word in a string with another word.
+
+
+
+
+
+
