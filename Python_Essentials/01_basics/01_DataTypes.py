1f5131f915f3cf69dc217931d71b04f9b85bf1b2
Ajay_Kumar <45093975+ajaaykumar@users.noreply.github.com>
2024-07-04 18:03:59 +0000

    conversions

--- Python_Essentials/01_basics/01_DataTypes.py
+++ Python_Essentials/01_basics/01_DataTypes.py
@@ -1,5 +1,17 @@
 
+#  type conversion
+integer_number = 123
+float_number = 1.23
 
+new_num = integer_number + float_number
+
+print(new_num)
+
+num_string = '12'
+num_integer = 23
+
+new_sum = int(num_string) + num_integer
+print(new_sum)
 
 
 
